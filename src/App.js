7ea283103e4ac6cb95137c0d9438fefcfb847508
Rayen <frigui.rayene1999@gmail.com>
2022-02-25 23:55:24 +0100

    landingpage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { BrowserRouter as Router, Route,Switch} from "react-router-dom";
+import Header from "./components/header/Header";
 import Login from "./components/land/Login";
 
 export class App extends Component{
@@ -8,6 +9,7 @@ export class App extends Component{
             <Router>
                 <Switch>
                     <Route path="/" component={Login}></Route>
+                    <Route path="/" component={Header}></Route>
                 </Switch>
             </Router>
     )}
--- src/components/land/Login.js
+++ src/components/land/Login.js
@@ -1,39 +1,64 @@
 import React from 'react';
 import './login.css';
 <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"></link>
+
 function Login() {
   return (
     <div className='LandingPage'>
        <div className="Logo"><a href="#">Univer<span>City.</span></a></div>
        <div className="container">
-            <div className="selectText">Login as :</div>
-            <div className="selectUser">
+          <div className="selectText fadeIn first">Login as :</div>
+          <div className="selectUser">
 
-              <div class="card">
-                  <div class="box">
-                      <i class="fas fa-paint-brush"></i>
-                      <div class="text">Adiministration</div>
-                  </div>
+            <div class="card fadeIn second">
+                <div class="box">
+                    <i class="fas fa-paint-brush"></i>
+                    <div class="text">Adiministration</div>
+                </div>
+              </div>
+
+              <div class="card fadeIn third">
+                <div class="box">
+                    <i class="fas fa-paint-brush"></i>
+                    <div class="text">Enseignant</div>
+                </div>
+              </div>
+
+              <div class="card fadeIn foruth">
+                <div class="box">
+                    <i class="fas fa-paint-brush"></i>
+                    <div class="text">Etudiant</div>
+                </div>
+              </div>
+          </div>
+          
+        </div>
+
+        <div className="wrapper fadeInDown">
+              <div id="formContent">
+              
+                <div className="fadeIn first">
+                  <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
                 </div>
 
-                <div class="card">
-                  <div class="box">
-                      <i class="fas fa-paint-brush"></i>
-                      <div class="text">Enseignant</div>
-                  </div>
+               
+                <form>
+                  <input type="text" id="login" class="fadeIn second" name="login" placeHolder="login"/>
+                  <input type="text" id="password" class="fadeIn third" name="login" placeHolder="password"/>
+                  <input type="submit" class="fadeIn fourth" value="Log In"/>
+                </form>
+
+              
+                <div id="formFooter">
+                  <a class="underlineHover" href="#">Forgot Password?</a>
                 </div>
 
-                <div class="card">
-                  <div class="box">
-                      <i class="fas fa-paint-brush"></i>
-                      <div class="text">Etudiant</div>
-                  </div>
-                </div>
+              </div>
+          </div>
+      </div>
 
-            </div>
-       </div>
-    </div>
+      
   )
+  
 }
-
 export default Login;
